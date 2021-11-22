abd473ea5f6fc80af85eed432b61d47c126e867b
Joulik <leroy.prof.SPC@gmail.com>
2021-11-22 22:01:32 +0100

    Ajoutez des fichiers projet.

---
+++ XamarinFormsWorkingTime/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsWorkingTime
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
